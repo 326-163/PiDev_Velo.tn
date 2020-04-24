d6858bc0a4b5caee93b33cb3091e4f962fc32aa9
Nahawand <nahawand.laajili@gmail.com>
2020-04-24 17:23:17 +0100

    {InsertMessage}

--- src/RentBundle/Controller/LocationController.php
+++ src/RentBundle/Controller/LocationController.php
@@ -24,23 +24,23 @@ class LocationController extends Controller
     /**
      * Home.
      *
-    */
+     */
     public function indexAction()
     {
-      //$user =$this->getUser();
-      //if($user){
+        //$user =$this->getUser();
+        //if($user){
         $em = $this->getDoctrine()->getManager();
         $locations = $em->getRepository('RentBundle:Location')->findAll();
-    //}else{
-      //  $this->redirectToRoute('fos_user_security_login');
- //}
+        //}else{
+        //  $this->redirectToRoute('fos_user_security_login');
+        //}
         return $this->render('RentBundle:location:location.html.twig', array(
             'locations' => $locations,
         ));
-        var_dump($locations); 
+        var_dump($locations);
         die();
     }
-    
+
     /**
      * Creates a new location entity.
      *
@@ -79,7 +79,7 @@ class LocationController extends Controller
     {
       $user =$this->getUser();
        if($user){
-        
+
       $em=$this->getDoctrine()->getEntityManager();
       $location = new Location();
        }else{
@@ -94,9 +94,7 @@ class LocationController extends Controller
       ->add('dateCreation',DateTimeType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
       ->add('Submit',SubmitType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
       ->getForm();
-
  $form->handleRequest($request);
-
  if ($form->isSubmitted() && $form->isValid() ) {
    $location->setTitre($form['titre']->getData());
    $location->setLieu($form['lieu']->getData());
@@ -106,15 +104,12 @@ class LocationController extends Controller
    $location->setDateCreation($form['dateCreation']->getData());
    $em->persist ($location);
    $em->flush();
-
    $this->addFlash('success','your rent has been successfuly persisted !');
-
    return $this->redirect ($this->generateUrl('location_confirmation'));
  }
-
 return $this->render('RentBundle:location:new.html.twig', array (
   'form'=>$form->createView()
-)) ; 
+)) ;
     }*/
 
     /**
@@ -130,62 +125,61 @@ return $this->render('RentBundle:location:new.html.twig', array (
             'location' => $location,
         ));
     }
-  
+
     /**
      * Displays a form to edit an existing location entity.
      *
      */
     public function editAction(Request $request, $id)
     {
-   /* $location = $this->getDoctrine()->getRepository(Location::class)->find($id);
-    $form = $this->createForm('RentBundle\Form\LocationType', $location);
-    $form->handleRequest($request);
-    if ($form->isSubmitted()) {
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($location);
-        $em->flush();
-        $this->addFlash('success','your rent has been successfuly updated !');
+        /* $location = $this->getDoctrine()->getRepository(Location::class)->find($id);
+         $form = $this->createForm('RentBundle\Form\LocationType', $location);
+         $form->handleRequest($request);
+         if ($form->isSubmitted()) {
+             $em = $this->getDoctrine()->getManager();
+             $em->persist($location);
+             $em->flush();
+             $this->addFlash('success','your rent has been successfuly updated !');
+             return $this->redirect ($this->generateUrl('location_show'));
+         }*/
 
-        return $this->redirect ($this->generateUrl('location_show'));
-    }*/
-    
 
 
         $em=$this->getDoctrine()->getEntityManager();
-      $location = $em->getRepository('RentBundle:Location')->find($id);
+        $location = $em->getRepository('RentBundle:Location')->find($id);
 
-      $form=$this->createFormBuilder($location)
-      ->add('titre',TextType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
-      ->add('lieu',TextType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
-      ->add('prix',IntegerType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
-      ->add('photo',TextType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
-      //->add('rating',IntegerType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
-      ->add('dateCreation',DateTimeType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
-      ->add('Enregistrer',SubmitType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
-      ->getForm();
+        $form=$this->createFormBuilder($location)
+            ->add('titre',TextType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+            ->add('lieu',TextType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+            ->add('prix',IntegerType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+            ->add('photo',TextType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+            //->add('rating',IntegerType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+            ->add('dateCreation',DateTimeType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+            ->add('Enregistrer',SubmitType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+            ->getForm();
 
- $form->handleRequest($request);
+        $form->handleRequest($request);
 
- if ($form->isSubmitted () ) {
-        $location->setTitre($form['titre']->getData());
-        $location->setLieu($form['lieu']->getData());
-        $location->setPrix($form['prix']->getData());
-        $location->setPhoto($form['photo']->getData());
-        //$location->setRating($form['rating']->getData() );
-        $location->setDateCreation($form['dateCreation']->getData());
-        $em->persist ($location);
-        $em->flush();
+        if ($form->isSubmitted () ) {
+            $location->setTitre($form['titre']->getData());
+            $location->setLieu($form['lieu']->getData());
+            $location->setPrix($form['prix']->getData());
+            $location->setPhoto($form['photo']->getData());
+            //$location->setRating($form['rating']->getData() );
+            $location->setDateCreation($form['dateCreation']->getData());
+            $em->persist ($location);
+            $em->flush();
 
-   $this->addFlash('success','your rent has been successfuly persisted !');
+            $this->addFlash('success','your rent has been successfuly persisted !');
 
-   return $this->redirect ($this->generateUrl('location_confirmation'));
- }
+            return $this->redirect ($this->generateUrl('location_confirmation'));
+        }
 
-return $this->render('RentBundle:location:edit.html.twig', array (
-  'form'=>$form->createView()
-)) ; 
+        return $this->render('RentBundle:location:edit.html.twig', array (
+            'form'=>$form->createView()
+        )) ;
     }
-    
+
 
     /**
      * Deletes a location entity.
@@ -195,13 +189,13 @@ return $this->render('RentBundle:location:edit.html.twig', array (
     {
         $em=$this->getDoctrine()->getEntityManager();
         $location = $em->getRepository('RentBundle:Location')->find($id);
-       
+
         $em->remove($location);
         $em->flush();
         return $this->redirectToRoute('location_homepage');
     }
-   
-   
+
+
     /**
      * Creates a form to delete a location entity.
      *
@@ -215,9 +209,9 @@ return $this->render('RentBundle:location:edit.html.twig', array (
             ->setAction($this->generateUrl('location_delete', array('id' => $location->getId())))
             ->setMethod('DELETE')
             ->getForm()
-        ;
+            ;
     }
-     /**
+    /**
      * confirmation.
      *
      */
@@ -234,51 +228,49 @@ return $this->render('RentBundle:location:edit.html.twig', array (
         ('location'=>$location));
 
     }
-    
-   /* public function rechercheAction(Request $request)
-    {
-         $em=$this->getDoctrine()->getManager();
-         $find=$em->getRepository(Location::class)->findAll();
-         if ($request->isMethod('POST')){
-             $type=$request->get('titre');
-             $find=$em->getRepository(Location::class)->findBy(array('titre'=>$titre));
-         }
-        return $this->render("RentBundle:location:show.html.twig", array(
-            "titre" => $titre
-        ));
-    }
-    
-     
-    /*public function getelementAction($id){
-        $club = $this->getDoctrine()->getRepository(Club::class);
-        $re =$club->findonebyname($id);
-        //dump($re);
-        return $this->render('@Club/Club/search.html.twig',
-            ['club'=>$re]);
 
-    }
-
-     
-    public function SendEmailAction(){
-        $message = \Swift_Message::newInstance()
-        ->setSubject('Formalab')
-        ->setFrom('nahawand.laajili@gmail.com')
-        ->setTo('nahawand.laajili@gmail.com')
-        ->setBody('test from nahawand');
-        $this->get('mailer')
-            ->send($message);
-        return $this->render('send.html.twig');
-     }
- 
-     
-     public function countaction ()
+    /* public function rechercheAction(Request $request)
      {
-       // yay! Use this to see if the user is logged in
- if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
-   throw $this->createAccessDeniedException();
- }
- }
-    */
+          $em=$this->getDoctrine()->getManager();
+          $find=$em->getRepository(Location::class)->findAll();
+          if ($request->isMethod('POST')){
+              $type=$request->get('titre');
+              $find=$em->getRepository(Location::class)->findBy(array('titre'=>$titre));
+          }
+         return $this->render("RentBundle:location:show.html.twig", array(
+             "titre" => $titre
+         ));
+     }
+
+
+     /*public function getelementAction($id){
+         $club = $this->getDoctrine()->getRepository(Club::class);
+         $re =$club->findonebyname($id);
+         //dump($re);
+         return $this->render('@Club/Club/search.html.twig',
+             ['club'=>$re]);
+     }
+
+     public function SendEmailAction(){
+         $message = \Swift_Message::newInstance()
+         ->setSubject('Formalab')
+         ->setFrom('nahawand.laajili@gmail.com')
+         ->setTo('nahawand.laajili@gmail.com')
+         ->setBody('test from nahawand');
+         $this->get('mailer')
+             ->send($message);
+         return $this->render('send.html.twig');
+      }
+
+
+      public function countaction ()
+      {
+        // yay! Use this to see if the user is logged in
+  if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+    throw $this->createAccessDeniedException();
+  }
+  }
+     */
     public function highdateAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
